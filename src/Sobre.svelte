5600a100d6fc89ea78c80ae8a5bf3a3c4c453283
EmanoelLuiz01 <133930585+EmanoelLuiz01@users.noreply.github.com>
2023-07-05 21:37:27 -0300

    Update Sobre.svelte

--- src/Sobre.svelte
+++ src/Sobre.svelte
@@ -1,1 +1,6 @@
-<h1>Nome dos Criadores:</h1>
+<h2>Nome dos Criadores:</h2>
+    <h3>Andre Vinicius: front end e back end </h3>
+    <h3>Decio joab: front end </h3> 
+    <h3>Emanoel Luiz : front end e back end</h3> 
+    <h3> Guilherme Gomes : front end</h3>
+    <h3>Vinicius Guedes : front end</h3>
