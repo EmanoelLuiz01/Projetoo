c9832cc6ed517595b3fa492e6c8e0d84c44fc996
EmanoelLuiz01 <133930585+EmanoelLuiz01@users.noreply.github.com>
2023-07-05 21:36:28 -0300

    Update Jogar.svelte

--- src/Jogar.svelte
+++ src/Jogar.svelte
@@ -1,6 +1,62 @@
-<script>
+<style>
+    .background {
+      background-image: url("https://png.pngtree.com/png-vector/20220818/ourlarge/pngtree-sports-stadium-with-soccer-goal-vector-illustration-png-image_5890244.png");
+      background-size: cover;
+      background-position: center;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      grid-gap: 10px;
+    }
+  
+      .transparent-button {
+        background-color: transparent;
+        border: 2px solid green;
+        color: green;
+        padding: 10px;
+        cursor: pointer;
+			  width: 190px;
+        height: 110px;
+        font-size: 16px;
+    }
+  </style>
+  
+  <script>
+    let buttons = [
+      {
+        text: ''
+      },
+      {
+        text: ''
+      },
+      {
+        text: ''
+      },
+      {
+        text: ''
+      },
+      {
+        text: ''
+      },
+      {
+        text: ''
+      },
+      {
+        text: ''
+      },
+      {
+        text: ''
+      },
+    ];
+  
     let pontos = 0;
-    let attempts = 5;
+    let tentativas = 5;
   
     function shoot() {
       const result = Math.random() < 0.5; // Simula o chute do jogador
@@ -12,19 +68,57 @@
         alert("Defendido!");
       }
   
-      attempts -= 1;
+      tentativas -= 1;
   
-      if (attempts === 0) {
+      if (tentativas === 0) {
         alert("Fim de jogo. Pontuação final: " + pontos);
+        buttons = []; // Remover os botões restantes
       }
+    }
+  
+    function reiniciarJogo() {
+      pontos = 0;
+      tentativas = 5;
+      buttons = [
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+        {
+          text: ''
+        },
+      ]; // Restaurar os botões
     }
   </script>
   
   <main>
-    <h1>Jogo de Pênaltis</h1>
-  
-    {#if attempts > 0}
-      <button on:click={shoot}>Chutar</button>
-    {/if}
-
+    <div class="background">
+      {#if tentativas > 0}
+        <div class="grid">
+          {#each buttons as button}
+            <button class="transparent-button" on:click={shoot}>{button.text}</button>
+          {/each}
+        </div>
+      {:else}
+        <button on:click={reiniciarJogo}>Tente denovo</button>
+      {/if}
+    </div>
   </main>
+  
