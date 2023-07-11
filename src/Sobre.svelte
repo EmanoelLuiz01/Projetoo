d90d1f95479f02e400e6bfedfaf299d55385eb5f
Vinisz05 <132289176+Vinisz05@users.noreply.github.com>
2023-07-11 11:31:45 -0300

    Update Sobre.svelte

--- src/Sobre.svelte
+++ src/Sobre.svelte
@@ -4,3 +4,23 @@
     <h3>Emanoel Luiz : front end e back end</h3> 
     <h3> Guilherme Gomes : front end</h3>
     <h3>Vinicius Guedes : front end</h3>
+    <style>
+         .background {
+        background-image: url("https://i0.wp.com/geekpopnews.com.br/wp-content/uploads/2022/11/Copa-3.jpg?resize=800%2C600&ssl=1");
+        background-size: cover;
+        background-position: center;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+         }
+
+
+
+
+
+
+
+    </style>
+
