f5d1bddbe828ab61ec2e69f1e1306c87f3e8047c
Vinisz05 <132289176+Vinisz05@users.noreply.github.com>
2023-07-11 11:32:51 -0300

    Update Menu.svelte

--- src/Menu.svelte
+++ src/Menu.svelte
@@ -11,18 +11,37 @@
             <button on:click={() => {trocarEstadoDoJogo("jogar"); }} class="jogar">Jogar</button>
         </div>
         <div class="container">
-            <button on:click={() => trocarEstadoDoJogo("sobre")}>Sobre</button>
+            <button on:click={() => {trocarEstadoDoJogo("sobre");}} class = "Sobre">Sobre</button>
         </div>
     </div>
 </main>
 
 <style>
-
-    .container {
-        text-align: center;
+    .background {
+      background-image: url("https://img.quizur.com/f/img5c633775e6dd31.52777503.jpg?lastEdited=1550006143");
+      background-size: cover;
+      background-position: center;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
-
-    .jogar {
-        margin-bottom: 10px;
+  
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      grid-gap: 10px;
     }
+    
+
+  
+ .Sobre{
+   
+    color: black;
+ }
+    .jogar {  
+        color: black;
+    }
+   
+    
 </style>
